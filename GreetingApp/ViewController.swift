25aee5f0c9c67c98aa70c10e7ec8ded3eafe7fe0
KDaria <dkdva@mail.ru>
2024-07-29 20:35:02 +0200

    Initial Commit

---
+++ GreetingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreetingApp
+//
+//  Created by Дарья Кобелева on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

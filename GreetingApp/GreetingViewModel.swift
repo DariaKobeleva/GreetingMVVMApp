379b9964cc01fc1ed16f969b8a417d3f37c65ff8
KDaria <dkdva@mail.ru>
2024-07-30 12:39:13 +0200

    MVVM Example

---
+++ GreetingApp/GreetingViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  GreetingViewModel.swift
+//  GreetingApp
+//
+//  Created by Дарья Кобелева on 30.07.2024.
+//
+
+protocol GreetingViewModelProtocol {
+    var greeting: String? { get }
+    var greetingDidChange: ((GreetingViewModelProtocol) -> Void)? { get set }
+    init(person: Person)
+    func showGreeting()
+}
+
+class GreetingViewModel: GreetingViewModelProtocol {
+    var greeting: String? {
+        didSet {
+            greetingDidChange?(self)
+        }
+    }
+    
+    var greetingDidChange: ((any GreetingViewModelProtocol) -> Void)?
+    
+    private let person: Person
+    
+    required init(person: Person) {
+        self.person = person
+    }
+    
+    func showGreeting() {
+        greeting = "Hello, \(person.name) \(person.surname)!"
+    }
+    
+    
+}
